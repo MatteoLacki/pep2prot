a21cbf049f243fbafcd3ac348b8bfb1421383e1a
matteo <matteo.lacki@gmail.com>
2024-08-28 19:38:34 +0200

    no need to get personal ,but we are really fast.

--- pep2prot/graph_ops.py
+++ pep2prot/graph_ops.py
@@ -1,6 +1,7 @@
 import math
 import multiprocessing as mp
-from collections import Counter
+import typing
+from collections import Counter, defaultdict
 from math import inf
 
 import networkx as nx
@@ -11,8 +12,9 @@ import pandas as pd
 from numba_progress import ProgressBar
 from tqdm import tqdm
 
+import ahocorasick
 import furious_fastas as ff
-from pep2prot.misc import asserting_unique
+from pep2prot.misc import asserting_unique, is_nondecreasing
 from pep2prot.numpy_ops import check_arrays_equal, check_vecs_the_same
 
 
@@ -219,14 +221,16 @@ def get_protein_group_covers(
     return covers
 
 
-def get_minimal_protein_group_coverage(
+def get_minimal_protein_group_coverage_slow(
     peptides: list[str],
     fastas: list[tuple[str, str]],
     min_number_of_peptides: int = 3,
     cpu_cnt: int = mp.cpu_count(),
 ) -> tuple[pd.DataFrame, npt.NDArray]:
     """
-    Get the approximate minimal protein group cover of submitted peptides.
+    Get the approximate minimal protein group cover of submitted peptides slowly.
+
+    Explicitly constructs the adjacency matrix.
 
     Arguments:
         peptide_report (list[str]): A list of found peptide sequences.
@@ -318,3 +322,263 @@ def get_minimal_protein_group_coverage(
     ), "Some peptide groups had different number of peptides than anticipated."
 
     return protein_groups, adjacency_matrix
+
+
+@numba.njit(boundscheck=True)
+def make_fasta_mask(prot_lens: list[int]) -> npt.NDArray:
+    """
+    Make an array mapping each letter in a big concatenated fasta string to it fasta number in the order of appearance in the file.
+    """
+    fastas_mask = np.full(fill_value=-1, shape=sum(prot_lens) + len(prot_lens) - 1)
+    i = 0
+    for prot_id, prot_len in enumerate(prot_lens):
+        fastas_mask[i : i + prot_len] = prot_id
+        i += prot_len + 1
+    return fastas_mask
+
+
+def iter_prot_pep_edges(
+    big_fasta: str,
+    peptides: list[str],
+    prot_ids_in_big_fasta: npt.NDArray,
+    check_ok: bool = True,
+) -> typing.Iterator[tuple[int, int]]:
+    """
+    Iterate over the edges of the protein-peptide graph.
+
+    Arguments:
+        big_fasta (str): concatenated protein sequences separated by space.
+        peptides (list[str]): Observed peptide strings.
+        prot_ids_in_big_fasta (npt.NDArray): an array mapping each letter in a big concatenated fasta string to it fasta number in the order of appearance in the file.
+        check_ok (bool): Check the ahocorasick Automaton produces valid subsequences.
+
+    Yield:
+        tuple[int,int]: Protein and peptide id.
+    """
+    automaton = ahocorasick.Automaton()
+    for pep_id, pep in enumerate(peptides):
+        automaton.add_word(pep, pep_id)
+    automaton.make_automaton()
+    for end, pep_id in automaton.iter(big_fasta):
+        pep = peptides[pep_id]
+        start = end - len(pep) + 1
+        if check_ok:
+            assert (
+                big_fasta[start : end + 1] == pep
+            ), "ahocorasick.Automaton did not find the peptide."
+        prot_id = prot_ids_in_big_fasta[start]
+        yield int(prot_id), pep_id
+
+
+def make_protein_peptide_graph(
+    peptide_sequences: list[str],
+    protein_sequences: list[str],
+) -> dict[int, list[int]]:
+    """Make a protein-peptide bipartite graph.
+
+    Here one set of nodes consists of proteins, another of peptides, and edge corresponds to peptide sequence being a subsequence of a protein sequence.
+
+    Arguments:
+        peptide_sequences (list[str]): List of peptide sequences.
+        protein_sequences (list[str]): List of protein sequences.
+
+    Returns:
+        dict: The mapping of nodes to other nodes in a graph. Strictly negative numbers correspond to peptide ids. To get a proper peptide id, follow 'pep_id = -inv_pep_id - 1'.
+    """
+    prot_ids_in_big_fasta = make_fasta_mask(prot_lens=list(map(len, protein_sequences)))
+    big_fasta = " ".join(protein_sequences)
+    graph = defaultdict(list)
+    for prot_id, pep_id in iter_prot_pep_edges(
+        big_fasta, peptide_sequences, prot_ids_in_big_fasta
+    ):
+        neg_pep_id = -pep_id - 1
+        graph[prot_id].append(neg_pep_id)
+        graph[neg_pep_id].append(prot_id)
+    return dict(graph)
+
+
+def fill_peptide_counts(
+    proteins_peptide_cnt: npt.NDArray,
+    graph: dict[int, list[int]],
+) -> None:
+    """Fill `proteins_peptide_cnt` with counts of peptides corresponding to protein ids in graph."""
+    for prot_id, pep_ids in graph.items():
+        if prot_id >= 0:
+            proteins_peptide_cnt[prot_id] += len(pep_ids)
+
+
+def fill_protein_groups(
+    graph: dict[int, list[int]],
+    prot_to_protgroup: npt.NDArray,
+    starting_group: int = -1,
+) -> None:
+    pep_ids_to_protgroup: dict[tuple[int, ...], int] = {}
+    group = starting_group
+    for prot_id, pep_ids in graph.items():
+        if prot_id >= 0:  # proteins nonzero, peptides negative
+            pep_ids: tuple[int, ...] = tuple(pep_ids)
+            if not pep_ids in pep_ids_to_protgroup:
+                group += 1
+                pep_ids_to_protgroup[tuple(pep_ids)] = group
+                prot_to_protgroup[prot_id] = group
+            else:
+                prot_to_protgroup[prot_id] = pep_ids_to_protgroup[pep_ids]
+
+
+def get_protein_groups_df(proteins_with_enough_peptides: pd.DataFrame) -> pd.DataFrame:
+    """Aggregate protein info to protein groups summary."""
+    protein_groups = (
+        proteins_with_enough_peptides.reset_index()
+        .groupby("group")
+        .aggregate(
+            {
+                "prot_id": [
+                    list,
+                    "min",
+                ],
+                "peptide_cnt": asserting_unique,
+                "header": list,
+                "group": len,
+            }
+        )
+    )
+    protein_groups.columns = [
+        "prot_idxs",
+        "repr_prot_id",
+        "peptide_cnt",
+        "accessions",
+        "prot_in_group_cnt",
+    ]
+    return protein_groups
+
+
+def iter_pgrepr_pep_edges(
+    graph: dict[int, list[int]],
+    protein_groups_representitives: typing.Iterable[int],
+) -> typing.Iterator[tuple[int, int]]:
+    """Iterate over edges in the protein-peptide graph that correspond only to protein group representatives.
+
+    Typically, we choose the first available protein in a group as representative.
+    """
+    protein_groups_representitives = set(protein_groups_representitives)
+    for prot_id, pep_ids in graph.items():
+        if prot_id >= 0 and prot_id in protein_groups_representitives:
+            for pep_id in pep_ids:
+                yield prot_id, pep_id
+
+
+def get_protein_group_covers_2(
+    edges: list[tuple[int, int]],
+    cpu_cnt: int = mp.cpu_count(),
+    _progressbar_msg: str = "Covering Peptides with Protein Groups",
+) -> list[list[int]]:
+    """
+    Establish the minimal protein group cover of peptides covered by all protein groups.
+    """
+    pep_prot_graph = nx.Graph(edges)
+    pep_prot_subgraphs = [
+        pep_prot_graph.subgraph(cc).copy()
+        for cc in nx.connected_components(pep_prot_graph)
+    ]
+    if _progressbar_msg != "":
+        pep_prot_subgraphs = tqdm(pep_prot_subgraphs, desc=_progressbar_msg)
+    with mp.Pool(cpu_cnt) as pool:
+        covers = list(pool.map(get_protein_group_cover_greadily, pep_prot_subgraphs))
+
+    return covers
+
+
+def get_minimal_protein_group_coverage_fast(
+    peptides: list[str],
+    fastas: list[tuple[str, str]],
+    min_number_of_peptides: int = 3,
+    cpu_cnt: int = mp.cpu_count(),
+    _debug: bool = False,
+) -> (
+    pd.DataFrame
+    | tuple[pd.DataFrame, pd.DataFrame, dict[int, list[int]], list[list[int]]]
+):
+    """
+    Get the approximate minimal protein group cover of submitted peptides.
+
+    Using the Aho-Corasick algorithm underneath for peptide-to-protein graph build up.
+
+    Arguments:
+        peptide_report (list[str]): A list of found peptide sequences.
+        fastas (list[tuple[str,str]]): A list of tuples with header and protein sequence each.
+        min_number_of_peptides (int): Minimal number of peptides per protein to be even worthy of being considered a cover.
+        cpu_cnt (int): Number of workers in a multiprocessing Pool to be used.
+        _debug (bool): Return intermediate data structures.
+
+    Returns:
+        pd.DataFrame | tuple[pd.DataFrame, pd.DataFrame, dict[int,list[int]], list[list[int]]] : A table containing protein groups in the greadily approximated minimal protein group cover. Optionally, that and a data frame with protein calculations results, the peptide-protein graph, and the list of covers per connected component.
+    """
+
+    proteins = pd.DataFrame({"header": [header for header, sequence in fastas]})
+    proteins.index.name = "prot_id"
+
+    assert len(set(proteins.header)) == len(proteins), "Protein headers are not unique."
+
+    graph = make_protein_peptide_graph(
+        peptides, protein_sequences=[seq for _, seq in fastas]
+    )
+
+    proteins["peptide_cnt"] = 0
+    fill_peptide_counts(proteins.peptide_cnt.to_numpy(), graph)
+
+    proteins[
+        "group"
+    ] = (
+        starting_group
+    ) = -1  # sentinel: -1 means a protein was not matched with any peptide
+    fill_protein_groups(graph, proteins.group.to_numpy(), starting_group=starting_group)
+
+    proteins_with_enough_peptides = proteins[
+        proteins.peptide_cnt >= min_number_of_peptides
+    ]
+
+    protein_groups = get_protein_groups_df(proteins_with_enough_peptides)
+
+    # this could also factorize through connected components, so as to do multiple permutations and different covers.
+    representative_edges = list(
+        iter_pgrepr_pep_edges(graph, protein_groups.repr_prot_id)
+    )
+
+    protein_representatives_covers = get_protein_group_covers_2(
+        edges=representative_edges,
+        cpu_cnt=cpu_cnt,
+        _progressbar_msg="Covering Peptides with Protein Groups",
+    )
+
+    protein_representatives_cover = {
+        prot_id for cover in protein_representatives_covers for prot_id in cover
+    }
+
+    protein_groups = protein_groups[  # retaining only PGs in the cover
+        protein_groups.repr_prot_id.isin(protein_representatives_cover)
+    ].copy()
+
+    protein_groups["pep_ids"] = [
+        [-inv_pep_id - 1 for inv_pep_id in graph[prot_id]]
+        for prot_id in protein_groups.repr_prot_id
+    ]
+
+    assert all(
+        pep_cnt == len(pep_ids)
+        for pep_cnt, pep_ids in zip(protein_groups.peptide_cnt, protein_groups.pep_ids)
+    ), "Some peptide groups had different number of peptides than anticipated."
+
+    pep_ids_before_cover = set(
+        -inv_pep_id - 1 for prot_id, inv_pep_id in representative_edges
+    )
+    final_covered_pep_ids = set(
+        pep_id for pep_ids in protein_groups.pep_ids for pep_id in pep_ids
+    )
+    assert (
+        pep_ids_before_cover == final_covered_pep_ids
+    ), "Protein covering algorithm lost some peptides."
+
+    if _debug:
+        return protein_groups, proteins, graph, protein_representatives_covers
+
+    return protein_groups
--- pep2prot/misc.py
+++ pep2prot/misc.py
@@ -1,4 +1,17 @@
+import typing
+
+
 def asserting_unique(x):
     x = set(x)
     assert len(x) == 1
     return x.pop()
+
+
+def is_nondecreasing(xx: typing.Iterable) -> bool:
+    xx = iter(xx)
+    x_prev = next(xx)
+    for x in xx:
+        if x < x_prev:
+            return False
+        x_prev = x
+    return True
